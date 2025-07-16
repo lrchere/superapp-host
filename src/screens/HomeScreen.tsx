e15313a4a7b05fcf5e2a1a6814fe7d742aac42b7
lrchere <128253749+lrchere@users.noreply.github.com>
2025-07-16 12:15:39 -0300

    Add turbo module example

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,36 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
 import React from 'react';
 import CollectionGrid from '../components/CollectionGrid';
 
+import NativeLocalStorage from '../../specs/NativeLocalStorage';
+
+const EMPTY = '<empty>';
+
 const HomeScreen = () => {
+  const [value, setValue] = React.useState<string | null>(null);
+
+  const [editingValue, setEditingValue] = React.useState<string | null>(null);
+
+  React.useEffect(() => {
+    const storedValue = NativeLocalStorage?.getItem('myKey');
+    setValue(storedValue ?? '');
+  }, []);
+
+  function saveValue() {
+    NativeLocalStorage?.setItem(editingValue ?? EMPTY, 'myKey');
+    setValue(editingValue);
+  }
+
+  function clearAll() {
+    NativeLocalStorage?.clear();
+    setValue('');
+  }
+
+  function deleteValue() {
+    NativeLocalStorage?.removeItem('myKey');
+    setValue('');
+  }
+
   return (
     <View style={styles.container}>
       <CollectionGrid
@@ -19,6 +47,17 @@ const HomeScreen = () => {
           },
         ]}
       />
+      <Text style={styles.text}>
+        Current stored value is: {value ?? 'No Value'}
+      </Text>
+      <TextInput
+        placeholder="Enter the text you want to store"
+        style={styles.textInput}
+        onChangeText={setEditingValue}
+      />
+      <Button title="Save" onPress={saveValue} />
+      <Button title="Delete" onPress={deleteValue} />
+      <Button title="Clear" onPress={clearAll} />
     </View>
   );
 };
@@ -27,6 +66,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 8,
+  },
+  text: {
+    fontSize: 16,
+    margin: 8,
+  },
+  textInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 8,
+    paddingHorizontal: 8,
   },
 });
 
